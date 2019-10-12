50d80ef7a6bfbc8da94e3d1db8f46c896f3f402f
Delfio <delfio_eu@hotmail.com>
2019-10-12 00:50:12 -0400

    Inicio de rotas privadas

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,4 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
+//Criando logica de rota privada
+export default function RouteWrapper({
+  component: Component,
+  isPrivate = false,
+  ...rest
+}) {
+  //Informação para saber se o usuario está logado
+  const signed = false;
 
-export default function RouteWrapper
+  //Não está logado e a rota é privada - redirecionar para home
+  if(!signed && isPrivate){
+    return <Redirect to ="/" />;
+  }
+
+  //Esta logado e a rota é privada - redireciona para dashboard
+  // Não faz sentido ele fazer login novamente por isso o "não privado"
+  if(signed && !isPrivate){
+    return <Redirect to ="Dashboard" />
+  }
+
+  return (
+    <Route
+      { ...rest }
+      component={Component}
+      />
+  )
+}
+
+//Validações de proptypes
+RouteWrapper.prototype = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps={
+  isPrivate: false,
+}
