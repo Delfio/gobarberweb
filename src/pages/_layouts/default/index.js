a327d2cbff5c081ccb7e03c9ee72ca8f5163173b
Delfio <delfio_eu@hotmail.com>
2019-10-26 23:10:11 -0400

    NavHeader com notificações estáticas

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropyTypes from 'prop-types';
 
+import Header from '~/components/Header';
+
 import { Wrapper } from './styles';
 
 //Todo componente inserido dentro do <AuthLayout> vem como propriedade no "children"
 export default function DefaultLayout({ children }) {
   return (
     <Wrapper>
+      <Header />
       {children}
     </Wrapper>
   );
