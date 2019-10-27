dcd6fa3774f6c9bb790784f55ee48193c78f2a46
Delfio <delfio_eu@hotmail.com>
2019-10-27 00:32:50 -0400

    Carregando os dados no perfil do usuario

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data){
+
+  }
+
   return (
-    <h1> Profile </h1>
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name ="name" placeholder ="Nome completo" />
+        <Input name ="email" type="email" placeholder ="Seu Email" />
+
+        <hr/>
+
+        <Input name ="oldpassword" type="password" placeholder="Sua Senha atual" />
+        <Input name ="password" type="password" placeholder="Nova Senha" />
+        <Input name ="confirmPassword" type="password" placeholder="Confirmação de senha" />
+
+        <button type="submit"> Atualizar Perfil </button>
+
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
   );
 }
